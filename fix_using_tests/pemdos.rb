d99de4b4325ffca411bb1cd81d76f5483970b616
Ben Dunjay <bendunjay@outlook.com>
2020-02-27 14:07:28 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,11 +1,6 @@
 def snake_it_up(string)
   if string[0] == "s"
-    new_string = string.split("")
-    
-  new_string[0]*10 + string
-  new_string.join("")
-  binding.pry
-  new_string
+  string[0]*10 + string
   else
   string
   end
